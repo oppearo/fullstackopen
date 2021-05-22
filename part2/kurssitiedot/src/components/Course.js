8990ca893af8a9dc150e727ed7579c81db8425a5
oppearo <69357695+oppearo@users.noreply.github.com>
2021-05-22 22:30:54 +0300

    2.1-2.5

---
+++ part2/kurssitiedot/src/components/Course.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Header = ({ name }) => {
+  return <h2>{name}</h2>;
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  const parts = props.parts;
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const exercises = parts.map((part) => part.exercises);
+  const total = exercises.reduce((a, b) => a + b, 0);
+  return <b>total of {total} exercises</b>;
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
