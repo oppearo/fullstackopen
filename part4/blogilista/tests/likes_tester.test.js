36588d62d3f630b022320191e4fb27beb412f4be
Olli-Pekka Aro <69357695+oppearo@users.noreply.github.com>
2023-12-04 18:55:20 +0200

    4.5.

--- part4/blogilista/tests/likes_tester.test.js
+++ part4/blogilista/tests/likes_tester.test.js
@@ -1,6 +1,17 @@
 const listHelper = require('../utils/list_helper')
 const dummyList = require('./unitTestBlogList')
 
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
+
 test('dummy returns one', () => {
   const blogs = []
 
@@ -9,17 +20,6 @@ test('dummy returns one', () => {
 })
 
 describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    }
-  ]
-
   test('when list has only one blog equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
     expect(result).toBe(5)
@@ -33,5 +33,32 @@ describe('total likes', () => {
   test('of a bigger list is calculated correctly', () => {
     const result = listHelper.totalLikes(dummyList)
     expect(result).toBe(36)
+  })
+})
+
+describe('favourite blog', () => {
+  test('of an empty list returns an empty object', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toEqual({ title: 'dummy', author: 'dummy', likes: 0 })
+  })
+
+  test('when list has only one blog equals that blog', () => {
+    const expectedResult = {
+      title: listWithOneBlog[0].title,
+      author: listWithOneBlog[0].author,
+      likes: listWithOneBlog[0].likes
+    }
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual(expectedResult)
+  })
+
+  test('of a bigger list is found correctly', () => {
+    const expectedResult = {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      likes: 12
+    }
+    const result = listHelper.favoriteBlog(dummyList)
+    expect(result).toEqual(expectedResult)
   })
 })
--- part4/blogilista/utils/list_helper.js
+++ part4/blogilista/utils/list_helper.js
@@ -1,17 +1,28 @@
-const dummy = (blogs) => {
+const dummy = (array) => {
   return 1
 }
 
-const totalLikes = array => {
-  const reducer = (accumulator, currentValue) => {
+const totalLikes = (array) => {
+  const addLikes = (accumulator, currentValue) => {
     return accumulator + currentValue.likes
   }
   return array.length === 0
     ? 0
-    : array.reduce(reducer, 0)
+    : array.reduce(addLikes, 0)
+}
+
+const favoriteBlog = (array) => {
+  const getFavorite = (previous, current) => {
+    return (previous && previous.likes > current.likes) ? previous : current
+  }
+
+  let value = array.reduce(getFavorite, { title: 'dummy', author: 'dummy', likes: 0 })
+
+  return { title: value.title, author: value.author, likes: value.likes }
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
