a7c83a74cc9d64fe3f158883231ef8d0d6877c89
Olli-Pekka Aro <69357695+oppearo@users.noreply.github.com>
2023-12-10 21:05:30 +0200

    add 4.15

--- part4/blogilista/app.js
+++ part4/blogilista/app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 
 mongoose.set('strictQuery', false);
 
@@ -23,5 +24,6 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
