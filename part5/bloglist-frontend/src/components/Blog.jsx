52e49675d6f4a014ee68d6d18c68b50a564824d8
Olli-Pekka Aro <olli-pekka@identio.fi>
2024-06-17 15:27:26 +0300

    add 5.7.

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} by {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [additionalInfoVisible, setAdditionalInfoVisible] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  const hideWhenVisible = { display: additionalInfoVisible ? "none" : "" };
+  const showWhenVisible = { display: additionalInfoVisible ? "" : "none" };
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} by {blog.author}
+      <div style={hideWhenVisible}>
+        {" "}
+        <button onClick={() => setAdditionalInfoVisible(true)}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.url}
+        <p>
+          {" "}
+          {blog.likes} <button>like</button>{" "}
+        </p>
+        {blog.user.name}
+        <button onClick={() => setAdditionalInfoVisible(false)}>hide</button>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
