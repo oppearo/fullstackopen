ae3e8cb866d9869ed0672d130758977bd7c3d01c
Olli-Pekka Aro <oppearo@gmail.com>
2024-07-03 10:02:46 +0300

    add 6.16.-6.17.

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,7 +1,6 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { showNotification } from '../reducers/notificationReducer'
-import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
@@ -10,8 +9,7 @@ const AnecdoteForm = () => {
       event.preventDefault()
       const content = event.target.anecdote.value
       console.log('add', content)
-      const newAnecdote = await anecdoteService.createNew(content)
-      dispatch(createAnecdote(newAnecdote))
+      dispatch(createAnecdote(content))
       dispatch(showNotification(`you added a new anecdote: '${content}'`))
       event.target.anecdote.value = ''
     }
