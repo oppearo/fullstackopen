9931e713c4b2086768213a17f32364ab7b1f1c6d
Olli-Pekka Aro <oppearo@gmail.com>
2024-07-23 16:50:53 +0300

    add 9.10.

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import cors from "cors";
+import diagnosisRouter from "./routes/diagnoses";
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 const PORT = 3001;
@@ -12,6 +13,8 @@ app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
   res.send("pong");
 });
+
+app.use("/api/diagnoses", diagnosisRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
