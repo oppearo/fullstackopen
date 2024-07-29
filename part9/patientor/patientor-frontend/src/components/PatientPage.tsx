dbb93455ac4795cab17bb4d2f3f9f37e94d4bf84
Olli-Pekka Aro <oppearo@gmail.com>
2024-07-29 14:04:48 +0300

    add 9.23.

--- part9/patientor/patientor-frontend/src/components/PatientPage.tsx
+++ part9/patientor/patientor-frontend/src/components/PatientPage.tsx
@@ -2,6 +2,10 @@ import { Gender, Patient } from "../types";
 import FemaleIcon from "@mui/icons-material/Female";
 import MaleIcon from "@mui/icons-material/Male";
 import TransgenderIcon from "@mui/icons-material/Transgender";
+import { Box, Typography } from "@mui/material";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
 
 interface PatientPageProps {
   patient: Patient | undefined;
@@ -29,11 +33,33 @@ const PatientPage = ({ patient }: PatientPageProps) => {
 
   return (
     <div>
-      <h3>
-        {patient.name} {genderToIcon(patient.gender)}
-      </h3>
-      ssn: {patient.ssn} <br />
-      occupation: {patient.occupation} <br />
+      <Typography variant="h4">
+        <Box sx={{ fontWeight: "bold", m: 1 }}>
+          {patient.name} {genderToIcon(patient.gender)}
+        </Box>
+      </Typography>
+      <Typography>
+        ssn: {patient.ssn} <br />
+        occupation: {patient.occupation} <br />
+      </Typography>
+      <Typography variant="h5">entries</Typography>
+      <List>
+        {patient.entries.map((entry) => {
+          return (
+            <ListItem disablePadding key={entry.id}>
+              <ListItemText>{entry.date}</ListItemText>
+              <ListItemText sx={{ fontStyle: "italic" }}>
+                {entry.description}
+              </ListItemText>
+            </ListItem>
+          );
+        })}
+        {patient.entries.map((entry) => {
+          return entry.diagnosisCodes?.map((diagnosis) => {
+            return <ListItem key={diagnosis}>{diagnosis}</ListItem>;
+          });
+        })}
+      </List>
     </div>
   );
 };
